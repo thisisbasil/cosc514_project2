ccca548c0470aa9bfeb4691d5b4542a3a38b0ac6
thisisbasil <bahuffma@gmail.com>
2021-04-20 11:17:16 -0400

    added random number generator

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
 #include "System.h"
-
+#include <random>
 using namespace std;
+
+
+// generates a random number between min and max
+// default values are between 0 and 100
+int rng(int min = 0, int max = 100)
+{
+	// declare an engine to seed
+    static default_random_engine re {};
+
+	// give it a uniform distribution
+	static uniform_int_distribution<int> dist;
+
+	// generate the number and return
+	return dist(re, uniform_int_distribtion<int>param_type { min, max };
+}
 
 int main()
 {
     System cpu;
+
+	while(true)
+	{
+		
+	}
+
     return 0;
 }
